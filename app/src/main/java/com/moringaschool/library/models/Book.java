5a6462f312fadc4e37f4b77bcda1c1b8586e1aa7
Joseph-beep-tech <Josephcluff006@gmail.com>
2021-01-19 21:12:13 +0300

    book method

--- app/src/main/java/com/moringaschool/library/models/Book.java
+++ app/src/main/java/com/moringaschool/library/models/Book.java
@@ -5,4 +5,12 @@ public class Book {
     private String name;
     private String description;
     private String rating;
+    private int nb_pages;
+    private String categorie;
+    private String publisher;
+    private String image_url;
+
+    public Book(){
+
+    }
 }
